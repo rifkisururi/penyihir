7b6891f54ac8b482db4fe81d64d40f8e1cbb61bd
rifki ahmad sururi <57984062+rifkisururi@users.noreply.github.com>
2021-02-16 23:51:25 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 */
 
 $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "https");
-$config['base_url'] .= "https://" . $_SERVER['HTTP_HOST'];
+$config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
 $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 
 /*
